38aa1f20fb33b24cbc26a896e6a885b828b739e8
Abdulla Sakallah <iaboodws11@gmail.com>
2022-03-06 23:44:19 -0600

    Create complex.c

---
+++ complex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //initializing i
+    int i=4;
+
+    //place holder
+    int x = 2i+1;
+
+    // The gcc compiler reads i as an imaginary number
+
+    printf("The result is: %d\n",x);
+
+
+    return 0;
+
+}
